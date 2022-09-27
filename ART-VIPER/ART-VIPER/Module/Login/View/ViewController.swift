eb04cd7bf7ccfc430603bf899e8fa3cea352e54a
Minh Tam <tamnm1996@gmail.com>
2022-09-27 19:00:01 +0700

    [INITIALIZATION] create new project

---
+++ ART-VIPER/ART-VIPER/Module/Login/View/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ART-VIPER
+//
+//  Created by Nguyen Minh Tam on 23/09/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
